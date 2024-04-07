167033e6ef70709a557804efbc00edd133884d69
shawn <150121812+GitHub-lovecc@users.noreply.github.com>
2024-04-07 15:48:21 +0800

    Update Second.sql

--- SQL/LearningRecord/Second.sql
+++ SQL/LearningRecord/Second.sql
@@ -1,1 +1,12 @@
+USE sql_store;
 
+SELECT * / 1, 2  -- 纵向筛选列，甚至可以是常数，可用AS关键字设置列别名（AS可省略），注意 DISTINCT 关键字的使用。
+FROM customers  -- 选择表
+WHERE customer_id < 4  -- 横向筛选行
+ORDER BY first_name  -- 排序
+
+-- 单行注释
+
+/*
+多行注释
+*/
