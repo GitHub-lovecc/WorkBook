3db5e0537f5d603d320de1302758c86ec4af3d05
shawn <150121812+GitHub-lovecc@users.noreply.github.com>
2024-04-09 20:34:35 +0800

    Create Introduction.py

---
+++ Python/MachineLearning/Introduction.py
@@ -0,0 +1,42 @@
+'''https://zhuanlan.zhihu.com/p/104580292'''
+广义的机器学习不仅包括有监督学习和无监督学习等基于海量数据的建模方法，还包括一系列在建模过程中提升模型准确率、
+降低降低模型复杂度和提高模型稳健性等的一系列方法，比如集成学习、强化学习、迁移学习和分布式学习等。
+
+有监督学习
+根据自变量和因变量数据的对应关系，生成一个函数构建从自变量到因变量的映射关系。
+根据因变量是否离散，我们将有监督学习任务划分为“分类”学习任务和“回归”学习任务。
+
+“分类”学习算法包括朴素贝叶斯、决策树、支持向量机、逻辑回归等。
+“回归”学习算法包括多项式回归、逐步回归、套索回归和岭回归等。
+基于回归树的集成算法可以同时处理“分类”任务和“回归”任务，包括随机森林、xgboost和lightgbm等。
+
+无监督学习
+直接对输入的数据进行建模，主要包括“降维”学习任务和“聚类”学习任务。
+“降维”算法包括因子分析、主成分分析、随机森林等。
+“聚类”算法包括k-means聚类、层次聚类和密度聚类等。
+
+建模：从实际问题落地到公式和代码。
+最优化：最小化目标函数求解参数。
+
+Generalized machine learning not only includes supervised learning and unsupervised learning based on massive data modeling methods, 
+but also includes a series of methods to improve the model accuracy in the modeling process.
+A series of methods to reduce model complexity and improve model robustness, such as integrated learning, reinforcement learning, 
+transfer learning and distributed learning.
+
+supervised learning
+According to the corresponding relationship between independent variable and dependent variable data, 
+a function is generated to construct the mapping relationship from independent variable to dependent variable.
+According to whether the dependent variable is discrete or not, 
+we divide the supervised learning task into "classification" learning task and "regression" learning task.
+
+Classification learning algorithms include naive Bayes, decision tree, support vector machine, logistic regression and so on.
+"Regression" learning algorithms include polynomial regression, stepwise regression, lasso regression and ridge regression.
+The ensemble algorithm based on regression tree can handle both "classification" and "regression" tasks, including random forest, xgboost and lightgbm.
+
+Unsupervised learning
+Modeling the input data directly mainly includes "dimension reduction" learning task and "clustering" learning task.
+"Dimension reduction" algorithms include factor analysis, principal component analysis, random forest and so on.
+"Clustering" algorithms include k-means clustering, hierarchical clustering and density clustering.
+
+Modeling: from practical problems to formulas and codes.
+Optimization: Minimize the solution parameters of the objective function.
